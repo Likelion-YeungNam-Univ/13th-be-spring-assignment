911177e8613fd035c639db2c2f8f2f8aab46b2c6
chamingyeong <mingyeong1104@naver.com>
2025-05-01 20:16:05 +0900

    feat controller/StudentController

--- src/main/java/com/example/springsession/controller/StudentController.java
+++ src/main/java/com/example/springsession/controller/StudentController.java
@@ -1,13 +1,60 @@
-package com.example.springsession;
+package com.example.springsession.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springsession.model.Student;
+import com.example.springsession.repository.StudentRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/students")
 public class StudentController {
-    @GetMapping("/students")
-    public String getStudent() {
-        System.out.println("GET/student");
-        return "student";
+
+    private StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Student> getStudents() {
+        System.out.println("GET/students/");
+        return studentRepository.readAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        System.out.println("GET/students/" + id);
+        return studentRepository.readById(id);
+    }
+
+    @PostMapping("/post")
+    public Long postStudent(@RequestBody Student student) {
+        System.out.println("POST/students/" + student);
+        return studentRepository.create(student);
+    }
+
+    @PutMapping("/{id}")
+    public Long putStudent(@PathVariable Long id, @RequestBody Student student){
+        System.out.println("PUT/students/" + id);
+        return studentRepository.updateAll(id, student);
+    }
+
+    @PatchMapping("/{id}")
+    public Long patchStudent(@PathVariable Long id, @RequestBody Student student){
+        System.out.println("PATCH/students/" + id);
+        return studentRepository.update(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public Long deleteStudentById(@PathVariable Long id) {
+        System.out.println("DELETE/students/" + id);
+        return studentRepository.deleteById(id);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteAllStudent() {
+        System.out.println("DELETE/students/");
+        studentRepository.deleteAllById();
     }
 }
